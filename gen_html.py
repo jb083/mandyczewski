35d208950681f167590e421bc940aceb7a6966c1
jb083 <sg.jb115@outlook.com>
2019-01-12 21:18:58 +0900

    update: "chapter" support

--- gen_html.py
+++ gen_html.py
@@ -1,10 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+import toml
 import copy
 import lxml.html
 import subprocess
 import multiprocessing as mp
+
+def read_summary():
+    # 文書データの読み込み
+    if not os.path.exists("SUMMARY.toml"):
+        print("Error: can not find `SUMMARY.toml` in this directory.")
+        sys.exit()
+    with open("SUMMARY.toml","r") as f:
+        summary = toml.load(f)
+    return summary
+
+
+def file_list(summary):
+    if "main" in summary:
+        return summary["main"]
+    elif "chapter" in summary:
+        fl = []
+        for c in summary["chapter"]:
+            fl.extend(c)
+        return fl
+    else:
+        print("Error: `SUMMARY.toml` must have `main` or `chapter`, but can not find it.")
+        print( summary )
+        sys.exit()
+
 
 ### ツール群 ###
 
@@ -28,80 +55,91 @@ def pandoc_convert(fp):
     subprocess.run(["pandoc", fp, "-t", "html5", "-s", "--mathml", "--section-divs",
                         "--css=../css/style.css", "-o", "../html/"+fp[:-2]+"html" ])
 
-def pandoc(file_list):
+def pandoc(summary):
     # pandoc による html ソースの生成
-    pool = mp.Pool(len(file_list))
-    pool.map(pandoc_convert, file_list)
+    fl = file_list(summary)
+    pool = mp.Pool(len(fl))
+    pool.map(pandoc_convert, fl)
 
 
-def format_html(file_list):
+def format_html_file(fp, sec, dic):
+    with open(fp,"r",encoding="utf-8") as f:
+        src = lxml.html.fromstring(f.read())
+        
+    # head>title の情報を補う
+    src.xpath("//title")[0].text = src.xpath("//h1")[0].text
+
+    # <p> タグを <div> タグに変換
+    for dom in src.xpath("//p"):
+        dom.tag = "div"
+        dom.attrib["class"] = "paragraph"
+
+    # セクション番号を付加
+    for section in src.xpath("//section[@class='level1']"):
+        text = section[0].text
+        section[0].text = "{}. {}".format(sec, text)
+        dic[section.attrib["id"]] = [ fp, "{}".format(sec) ]
+    for subsec, section in enumerate(src.xpath("//section[@class='level2']")):
+        text = section[0].text
+        if "{}".format(text) != "None":
+            section[0].text = "{}-{}. {}".format(sec, subsec+1, text)
+        else:
+            section[0].text = "{}-{}. ".format(sec, subsec+1)
+        dic[section.attrib["id"]] = [ fp, "{}-{}".format(sec, subsec+1) ]
+                
+    # 独立した数式を <div> で囲い数式番号を付加
+    l = 0
+    for math in src.xpath("//math[@display='block']"):
+        eq = copy.copy(math)
+        eq.tail = ""
+        # もともとあった要素を除去
+        math.tag = "div"
+        clear_dom(math)
+        # 数式を追加
+        math.attrib["class"] = "matheq"
+        math.append(lxml.html.Element("span"))
+        math[0].attrib["class"] = "meq"
+        math[0].append(eq)
+        # 数式番号を追加
+        math.append(lxml.html.Element("span"))
+        math[1].attrib["class"] = "mnumber"
+        if math.getnext() is not None:
+            if math.getnext().tag == "nolabel":
+                continue
+            l += 1
+            math[1].text = "({}.{})".format(sec.replace("-","."),l)
+
+    # 数式番号の名称を記録
+    for label in src.xpath("//label"):
+        eqlabel = label.attrib["id"]
+        target = label.getprevious()
+        target[0].attrib["id"] = eqlabel
+        dic[eqlabel] = [ fp, target[1].text ]
+        label.drop_tag()
+
+    # ファイル書き出し
+    write_src(src, fp)
+
+
+def format_html(summary):
     # 各 html ファイルの形を整える
     dic = {} # 数式番号の参照を解決するための辞書
-    for sec, fp in enumerate(file_list):
-        sec += 1
-        fp = "../html/"+fp[:-2]+"html"
-        with open(fp,"r",encoding="utf-8") as f:
-            src = lxml.html.fromstring(f.read())
-        
-        # head>title の情報を補う
-        src.xpath("//title")[0].text = src.xpath("//h1")[0].text
-
-        # <p> タグを <div> タグに変換
-        for dom in src.xpath("//p"):
-            dom.tag = "div"
-            dom.attrib["class"] = "paragraph"
-
-        # セクション番号を付加
-        for section in src.xpath("//section[@class='level1']"):
-            text = section[0].text
-            section[0].text = "{}. {}".format(sec, text)
-            dic[section.attrib["id"]] = [ fp, "{}".format(sec) ]
-        for subsec, section in enumerate(src.xpath("//section[@class='level2']")):
-            text = section[0].text
-            if "{}".format(text) != "None":
-                section[0].text = "{}-{}. {}".format(sec, subsec+1, text)
-            else:
-                section[0].text = "{}-{}. ".format(sec, subsec+1)
-            dic[section.attrib["id"]] = [ fp, "{}-{}".format(sec, subsec+1) ]
-        
-        # 独立した数式を <div> で囲い数式番号を付加
-        l = 0
-        for math in src.xpath("//math[@display='block']"):
-            eq = copy.copy(math)
-            eq.tail = ""
-            # もともとあった要素を除去
-            math.tag = "div"
-            clear_dom(math)
-            # 数式を追加
-            math.attrib["class"] = "matheq"
-            math.append(lxml.html.Element("span"))
-            math[0].attrib["class"] = "meq"
-            math[0].append(eq)
-            # 数式番号を追加
-            math.append(lxml.html.Element("span"))
-            math[1].attrib["class"] = "mnumber"
-            if math.getnext() is not None:
-                if math.getnext().tag == "nolabel":
-                    continue
-            l += 1
-            math[1].text = "({}.{})".format(sec,l)
-
-        # 数式番号の名称を記録
-        for label in src.xpath("//label"):
-            eqlabel = label.attrib["id"]
-            target = label.getprevious()
-            target[0].attrib["id"] = eqlabel
-            dic[eqlabel] = [ fp, target[1].text ]
-            label.drop_tag()
-
-        # ファイル書き出し
-        src = lxml.html.tostring(src, encoding="utf-8",
-                                 doctype='<!DOCTYPE html>',
-                                 pretty_print=True ).decode("utf-8")
-        with open(fp,"w",encoding="utf-8") as f:
-            f.write( src )
-
-    for fp in file_list:
+    if "main" in summary:
+        for sec, fp in enumerate(summary["main"]):
+            sec += 1
+            fp = "../html/"+fp[:-2]+"html"
+            format_html_file(fp, "{}".format(sec), dic)
+    elif "chapter" in summary:
+        for cp, clist in enumerate(summary["chapter"]):
+            cp += 1
+            for sec, fp in enumerate(clist):
+                sec += 1
+                fp = "../html/"+fp[:-2]+"html"
+                format_html_file(fp, "{}-{}".format(cp,sec), dic)
+    else:
+        print("Error: `SUMMARY.toml` must have `main` or `chapter`, but can not find it.")
+        sys.exit()
+    for fp in file_list(summary):
         # 参照を解決
         fp = "../html/"+fp[:-2]+"html"
         with open(fp,"r",encoding="utf-8") as f:
@@ -112,12 +150,9 @@ def format_html(file_list):
             ref.attrib["href"] = dic[label][0]+"#{}".format(label)
             ref.text = dic[label][1]
         # ファイル書き出し
-        src = lxml.html.tostring(src, encoding="utf-8",
-                                 doctype='<!DOCTYPE html>',
-                                 pretty_print=True ).decode("utf-8")
-        with open(fp,"w",encoding="utf-8") as f:
-            f.write( src )
-            
+        write_src(src, fp)
+
+        
 
 def make_index(summary):
     # index.html の作成
@@ -139,12 +174,12 @@ def make_index(summary):
   </body>
 </html>''')
     # <title> タグ
-    index[0][1].text = summary["document"]["title"]
-    index[1][0][0].text = summary["document"]["title"]
+    index[0][1].text = summary["title"]
+    index[1][0][0].text = summary["title"]
 
     # 目次の作成
     ul = index[1][0][1]
-    for fp in summary["files"]["main"]:
+    for fp in file_list(summary):
         fp = "../html/"+fp[:-2]+"html"
         with open(fp, "r", encoding="utf-8") as f:
             src = lxml.html.fromstring(f.read())
@@ -165,8 +200,8 @@ def make_index(summary):
         ul.append(li)
 
     # 著作権表示
-    index[1][1][0].text = "©{} {}".format( summary["document"]["date"],
-                                           summary["document"]["author"] )
+    index[1][1][0].text = "©{} {}".format( summary["date"],
+                                           summary["author"] )
             
     with open("../index.html","w") as f:
         index = lxml.html.tostring(index, encoding="utf-8",
@@ -175,7 +210,8 @@ def make_index(summary):
         f.write(index)
 
 
-def make_footer(file_list, doc):
+def make_footer(summary):
+    fl = file_list(summary)
     # 各ファイルにフッターを生成
     section_names = []
     # セクション名を取得
@@ -183,7 +219,7 @@ def make_footer(file_list, doc):
         src = lxml.html.fromstring(f.read())
     for a in src.xpath("/html/body/article/ul/li/a"):
         section_names.append(a.text)
-    for i, fp in enumerate(file_list):
+    for i, fp in enumerate(fl):
         fp = "../html/"+fp[:-2]+"html"
         with open(fp,"r",encoding="utf-8") as f:
             src = lxml.html.fromstring(f.read())
@@ -201,7 +237,7 @@ def make_footer(file_list, doc):
         if i > 0:
             div.append(lxml.html.Element("a"))
             div[0].text = section_names[i-1]
-            div[0].attrib["href"] = file_list[i-1][:-2]+"html"
+            div[0].attrib["href"] = fl[i-1][:-2]+"html"
         footer[0].append( div )
         # 目次
         div = lxml.html.Element("div")
@@ -209,17 +245,17 @@ def make_footer(file_list, doc):
         div[0].text = "目次"
         div[0].attrib["href"] = "../index.html"
         footer[0].append( div )
-        if i < len(file_list)-1:
+        if i < len(fl)-1:
             # 次のセクション
             div = lxml.html.Element("div")
             div.append(lxml.html.Element("a"))
             div[0].text = section_names[i+1]
-            div[0].attrib["href"] = file_list[i+1][:-2]+"html"
+            div[0].attrib["href"] = fl[i+1][:-2]+"html"
             footer[0].append( div )
         # 著作権表示
         div = lxml.html.Element("div")
         div.attrib["class"] = "copyright"
-        div.text="©{} {}".format(doc["date"], doc["author"])
+        div.text="©{} {}".format(summary["date"], summary["author"])
         footer.append(div)
         
         # ファイル書き出し
