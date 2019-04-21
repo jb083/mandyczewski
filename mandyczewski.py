db07ec1a2760ed24f3c1a6c905324ed5a3e43c50
jb083 <sg.jb115@outlook.com>
2019-04-21 18:35:38 +0900

    update: load gen_html/*

--- mandyczewski.py
+++ mandyczewski.py
@@ -2,32 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from gen_html import *
 from new_note import new_note
 from deploy import deploy
 
-def gen_html():
-    # SUMMARY.toml の読み込み
-    summary = read_summary()
-    
-    # pandoc により HTML ファイルを生成
-    pandoc(summary)
-
-    # HTML ファイルを調整しセクション番号および数式番号を付加,
-    # 相対参照を解決してハイパーリンクを張る
-    format_html(summary)
-
-    # index.html を作成し, 各 HTML ファイルにフッターを付加
-    make_index(summary)
-    make_footer(summary)
-
 
 if __name__ == "__main__":
-    print("mandyczewski 0.1.7")
+    print("mandyczewski 0.2.0-dev0")
     
     if len(sys.argv) == 1:
+        from gen_html import gen_html
         gen_html()
     elif sys.argv[1] == "gen":
+        from gen_html import gen_html
         gen_html()
     elif sys.argv[1] == "new":
         new_note()
